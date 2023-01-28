860a51c2ac127d5838f2ad416afc1c7086c9abd3
KolyaKhmarjuk <kolyakhmarjuk@gmail.com>
2023-01-28 21:05:55 +0200

    push

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
 
-axios.defaults.baseURL =
-  'https://pixabay.com/api/?key=33125527-3d6befa9d5d1f6271bd5a7dac&';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-async function fetchData(item) {
+const KEY = '?key=33125527-3d6befa9d5d1f6271bd5a7dac';
+
+async function fetchData(search) {
   const res = await axios(
-    `q=${item}&image_type=photo&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
+    `${KEY}&q=${search}&image_type=photo&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
   );
 
   return res;
 }
 
-const fetchGalley = item => fetchData(item);
+const fetchGalley = search => fetchData(search);
 
 export { fetchGalley };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { markupList } from './markup/markupListGallery';
 import { refs } from './refs';
 import Notiflix from 'notiflix';
 
-let page = 1;
-
 const onSearch = e => {
   e.preventDefault();
   const input = e.currentTarget.searchQuery.value;
 
   fetchGalley(input);
+
+  console.log(input);
 };
 
 // const renderListImage = res => {
