fbbabc0fb180cce454cc18f96b8dbc17edd3edfc
KolyaKhmarjuk <kolyakhmarjuk@gmail.com>
2023-01-29 21:01:24 +0200

    push

--- src/markup/markupListGallery.js
+++ src/markup/markupListGallery.js
@@ -33,20 +33,24 @@ const getMarkupImage = response => {
         largeImageURL,
       }) =>
         `<div class="photo-card">
-        <img width="200" height "150" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <img width="300" height "150" src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
-          <p class="info-item">
-            <b>Likes: ${likes}</b>
+          <li class="info-item">
+            <b>Likes</b>
+            <p>${likes}</p>
+          </li>
+          <li class="info-item">
+            <b>Views</b>
+            <p>${views}</p>
           </p>
-          <p class="info-item">
-            <b>Views: ${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments: ${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads: ${downloads}</b>
-          </p>
+          <li class="info-item">
+            <b>Comments</b>
+            <p>${comments}</p>
+          </li>
+          <li class="info-item">
+            <b>Downloads</b>
+            <p>${downloads}</p>
+          </li>
         </div>
       </div>`
     )
