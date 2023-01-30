154bf4eb27a5a08b727442ca2c98e8006dd9dbce
KolyaKhmarjuk <kolyakhmarjuk@gmail.com>
2023-01-30 20:54:36 +0200

    push

--- src/index.js
+++ src/index.js
@@ -12,11 +12,21 @@ async function onSearch(e) {
   e.preventDefault();
   clearHitsList();
   apiService.query = e.currentTarget.elements.searchQuery.value;
+
   if (apiService.query === '') {
+    refs.loadMore.setAttribute('hidden', true);
     return Notify.info('Enter something to search');
   }
+
   apiService.resetPage();
   apiService.fetchData().then(appendHitsMarkup);
+
+  refs.loadMore.removeAttribute('hidden');
+  refs.loadMore.textContent = 'Загрузка...';
+
+  if (apiService.query.length > 1) {
+    refs.loadMore.textContent = 'Load more';
+  }
 }
 
 function onLoadMore() {
